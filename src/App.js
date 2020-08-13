616a56da4afe88f94b1076cbf5fc6967b5755bc0
Nick Celaya <nickcelaya07@gmail.com>
2020-08-13 15:56:50 -0600

    naive component layout

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-	const buttonText = "Click me";
-	const labelText = "Enter Name:";
-
 	return (
-		<div className='App'>
-			<label className='label' htmlFor='name'>
-				{labelText}
-			</label>
-			<input id='name' type='text' />
-			<button style={{ backgroundColor: "blue", color: "white" }}>
-				{buttonText}
-			</button>
-			test test
+		<div className='ui container comments'>
+			<div className='comment'>
+				<a href='/' className='avatar'>
+					<img alt='avatar' />
+				</a>
+				<div className='content'>
+					<a href='/' className='author'>
+						Diego
+					</a>
+				</div>
+				<div className='metadata'>
+					<span className='date'>Today at 6:00PM</span>
+				</div>
+				<div className='text'>Nice blog post!</div>
+			</div>
 		</div>
 	);
 };
