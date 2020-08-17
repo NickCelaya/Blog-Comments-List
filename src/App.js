81b3243eb7eeb33b27bff7a47adcc977c877769a
Nick Celaya <nickcelaya07@gmail.com>
2020-08-17 12:04:49 -0600

    Added props in parent component and passed them to child component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
 import React from "react";
-// import logo from "./logo.svg";
-// import Hello from "./components/Hello";
+import CommentDetail from "./CommentDetail";
 import "./App.css";
 import faker from "faker";
 
 const App = () => {
 	return (
 		<div className='ui container comments'>
-			<div className='comment'>
-				<a href='/' className='avatar'>
-					<img alt='avatar' src={faker.image.avatar()} />
-				</a>
-				<div className='content'>
-					<a href='/' className='author'>
-						Diego
-					</a>
-				</div>
-				<div className='metadata'>
-					<span className='date'>Today at 6:00PM</span>
-				</div>
-				<div className='text'>Nice blog post!</div>
-			</div>
-			<div className='comment'>
-				<a href='/' className='avatar'>
-					<img alt='avatar' src={faker.image.avatar()} />
-				</a>
-				<div className='content'>
-					<a href='/' className='author'>
-						Diego
-					</a>
-				</div>
-				<div className='metadata'>
-					<span className='date'>Today at 6:00PM</span>
-				</div>
-				<div className='text'>Nice blog post!</div>
-			</div>
+			<CommentDetail author='Nick' time='Today at 3:00am' comment="So hot!" avatar={faker.image.avatar()}/>
+			<CommentDetail author='Jessica' time='Today at 10:00am' comment="I like this post!" avatar={faker.image.avatar()}/>
+			<CommentDetail author='Ronan' time='Today at 5:00pm' comment="If you wrote a book I'd buy it" avatar={faker.image.avatar()}/>
 		</div>
 	);
 };
--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,26 +1,26 @@
 import React from "react";
-import faker from "faker";
+// import faker from "faker";
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+	console.log(props);
 	return (
 		<div>
 			<div className='comment'>
 				<a href='/' className='avatar'>
-					<img alt='avatar' src={faker.image.avatar()} />
+					<img alt='avatar' src={props.avatar} />
 				</a>
 				<div className='content'>
 					<a href='/' className='author'>
-						Diego
+						{props.author}
 					</a>
 				</div>
 				<div className='metadata'>
-					<span className='date'>Today at 6:00PM</span>
+					<span className='date'>{props.time}</span>
 				</div>
-				<div className='text'>Nice blog post!</div>
+				<div className='text'>{props.comment}</div>
 			</div>
 		</div>
 	);
 };
 
 export default CommentDetail;
- 
