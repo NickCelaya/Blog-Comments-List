32b35625c6ee0a6b81e726e828144daf717555d0
Nick Celaya <nickcelaya07@gmail.com>
2020-08-21 12:16:36 -0600

    Wrapped Approval card compnent around CommentDetail component in App.js then accessed CommentDetail inside ApprovalCard component as props.children

--- src/App.js
+++ src/App.js
@@ -7,25 +7,30 @@ import faker from "faker";
 const App = () => {
 	return (
 		<div className='ui container comments'>
-			<ApprovalCard />
-			<CommentDetail
-				author='Nick'
-				time='Today at 3:00am'
-				comment='So hot!'
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author='Jessica'
-				time='Today at 10:00am'
-				comment='I like this post!'
-				avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-				author='Ronan'
-				time='Today at 5:00pm'
-				comment="If you wrote a book I'd buy it"
-				avatar={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author='Nick'
+					time='Today at 3:00am'
+					comment='So hot!'
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author='Jessica'
+					time='Today at 10:00am'
+					comment='I like this post!'
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+					author='Ronan'
+					time='Today at 5:00pm'
+					comment="If you wrote a book I'd buy it"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	);
 };
--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
+	console.log(props);
 	return (
 		<div className='ui card'>
-			<div className='content'> ApprovalCard </div>
+			<div className='content'>{props.children}</div>
 			<div className='extra content'>
 				<div className='ui two buttons'>
 					<div className='ui basic green button'>Approve</div>
@@ -15,3 +16,7 @@ const ApprovalCard = () => {
 };
 
 export default ApprovalCard;
+
+//props.childern is the CommentDetail component. In App.js we wrapped the ApprovalCard component
+// around the CommentDetail making a parent child component relationship. which is why we can access CommentDetail
+// as props.children here in the ApprovalCard component.
