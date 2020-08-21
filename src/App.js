e267daa8fe5e3e9b6f66d556295476aef586c986
Nick Celaya <nickcelaya07@gmail.com>
2020-08-21 11:43:31 -0600

    Created Approval Card Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import "./App.css";
 import faker from "faker";
 
 const App = () => {
 	return (
 		<div className='ui container comments'>
-			<CommentDetail author='Nick' time='Today at 3:00am' comment="So hot!" avatar={faker.image.avatar()}/>
-			<CommentDetail author='Jessica' time='Today at 10:00am' comment="I like this post!" avatar={faker.image.avatar()}/>
-			<CommentDetail author='Ronan' time='Today at 5:00pm' comment="If you wrote a book I'd buy it" avatar={faker.image.avatar()}/>
+			<ApprovalCard />
+			<CommentDetail
+				author='Nick'
+				time='Today at 3:00am'
+				comment='So hot!'
+				avatar={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author='Jessica'
+				time='Today at 10:00am'
+				comment='I like this post!'
+				avatar={faker.image.avatar()}
+			/>
+			<CommentDetail
+				author='Ronan'
+				time='Today at 5:00pm'
+				comment="If you wrote a book I'd buy it"
+				avatar={faker.image.avatar()}
+			/>
 		</div>
 	);
 };
